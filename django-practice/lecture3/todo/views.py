f03cd755955b9311d824179dbaed8be1fb0995df
Eliska <eliska.achee@gmail.com>
2020-10-15 10:10:44 -0600

    Finished todo app

--- django-practice/lecture3/todo/views.py
+++ django-practice/lecture3/todo/views.py
@@ -1,9 +1,33 @@
+from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-tasks = ["get milk", "get vegetables", "get flour"]
+class NewTodoForm(forms.Form):
+    todo = forms.CharField(label="Add Todo")
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 # Create your views here.
 def index(request):
+    if "todos" not in request.session:
+        request.session["todos"] = []
+
     return render(request, "todo/index.html", {
-        "tasks": tasks
+        "todos": request.session["todos"]
+    })
+    
+def add(request):
+    if request.method == "POST":
+        form = NewTodoForm(request.POST)
+        if form.is_valid():
+            todo = form.cleaned_data["todo"]
+            request.session["todos"] += [todo]
+            return HttpResponseRedirect(reverse("todo:index"))
+        else:
+            return render(request, "todo/add.html", {
+                "form": form
+            })
+
+    return render(request, "todo/add.html", {
+        "form": NewTodoForm()
     })
